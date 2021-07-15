986838187b61dfc1db3719fa5c6663b43f0fb740
zhangyudong <977872239@qq.com>
2021-07-16 00:36:23 +0800

    7,一对多:创建2个数据表,可以保存但是外键为空没有关系

---
+++ src/test/java/OneToManyTest.java
@@ -0,0 +1,46 @@
+import cn.itcast.dao.CustomerDao;
+import cn.itcast.dao.LinkManDao;
+import cn.itcast.domain.Customer;
+import cn.itcast.domain.LinkMan;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class OneToManyTest {
+
+    @Autowired
+    private CustomerDao customerDao;
+
+    @Autowired
+    private LinkManDao linkManDao;
+
+    /**
+     * 保存一个客户，保存一个联系人
+     *  效果：客户和联系人作为独立的数据保存到数据库中
+     *      联系人的外键为空
+     *  原因？
+     *      实体类中没有配置关系
+     */
+    @Test
+    @Transactional //配置事务
+    @Rollback(false) //不自动回滚
+    public void testAdd() {
+        //创建一个客户，创建一个联系人
+        Customer customer = new Customer();
+        customer.setCustName("百度");
+
+        LinkMan linkMan = new LinkMan();
+        linkMan.setLkmName("小李");
+
+
+
+        customerDao.save(customer);
+        linkManDao.save(linkMan);
+    }
+}
