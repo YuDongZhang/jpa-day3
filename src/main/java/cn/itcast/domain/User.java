be8a9ea736cb19db9172c428e6816c6df847124e
YuDongZhang <977872239@qq.com>
2021-07-16 10:32:50 +0800

    15,基本的多对多框架

---
+++ src/main/java/cn/itcast/domain/User.java
@@ -0,0 +1,40 @@
+package cn.itcast.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "sys_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "age")
+    private Integer age;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
