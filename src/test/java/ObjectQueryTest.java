b6aece94a83434cc52fa3b74d7e9266f29203602
YuDongZhang <977872239@qq.com>
2021-07-16 16:39:09 +0800

    21,对象导航查询 : 发现还是没有立即去查询

---
+++ src/test/java/ObjectQueryTest.java
@@ -0,0 +1,57 @@
+import cn.itcast.dao.CustomerDao;
+import cn.itcast.dao.LinkManDao;
+import cn.itcast.dao.RoleDao;
+import cn.itcast.dao.UserDao;
+import cn.itcast.domain.Customer;
+import cn.itcast.domain.LinkMan;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class ObjectQueryTest {
+
+    @Autowired
+    private CustomerDao customerDao;
+
+    @Autowired
+    private LinkManDao linkManDao;
+
+    //could not initialize proxy - no Session
+    //测试对象导航查询(查询一个对象的时候,通过此对象查询所有的关联对象)
+    @Test
+    @Transactional//没有事务报错 , 添加
+    public void testQuery1(){
+        //查询id为1的客户
+        Customer customer = customerDao.getOne(1l);//延迟加载
+        //对象导航查询,此客户下的所有联系人
+        Set<LinkMan> linkMans = customer.getLinkMans();
+        for (LinkMan linkMan :
+                linkMans) {
+            System.out.println( linkMan);
+        }
+
+    }
+
+
+    @Test
+    @Transactional//没有事务报错 , 添加
+    public void testQuery2(){
+        //查询id为1的客户
+        Customer customer = customerDao.findOne(1l);
+        //对象导航查询,此客户下的所有联系人
+        Set<LinkMan> linkMans = customer.getLinkMans();
+        for (LinkMan linkMan :
+                linkMans) {
+            System.out.println( linkMan);
+        }
+
+    }
+
+}
